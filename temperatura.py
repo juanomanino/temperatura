86a5dbe8b8a7f31de8f64cc6fb6da2fac316692b
Juan Manuel Niño Piña <102763766+juanomanino@users.noreply.github.com>
2022-04-21 17:27:17 -0500

    Update temperatura.py

--- temperatura.py
+++ temperatura.py
@@ -3,16 +3,3 @@ Programa para calcular grados Kelvin y Celsius"""
 
 print("----------------------")
 print("---Grados Celsius a Fahrenheit y Kelvin-----")
-print("----------------------")
-
-#input
-c=int(input("Digite los grados Celsius: "))
-
-#processing
-k=c+273.15
-f=1.8*c+32
-
-#output
-print(str(c)+" grados Celsius son "+str(k)+" grados kelvin y "+str(f)+" grados fahrenheit")
-
-
