c6cd955d598a8f0d0506506e7b19add335dd6d6b
juanomanino <juannino@colegioguanenta.edu.co>
2022-04-21 16:30:33 -0500

    Desarrollo: creación de diagrama de flujo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Programa No 3: Convertir grados Celsius a Kelvin y Fahrenheit
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ temperatura.py
@@ -0,0 +1,8 @@
+"""Programa No. 3
+Programa para calcular grados Kelvin y Celsius"""
+
+print("----------------------")
+print("---Grados Celsius a Fahrenheit y Kelvin-----")
+print("----------------------")
+
+#input
